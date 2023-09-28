6e7edc4958eed108b3e6f14f19588fe5e14ef02d
Ezmuk <ezechielmukenge@gmail.com>
2023-09-28 12:08:31 +0200

    ajout des tableax et autres

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
+const greetings = ["Bonjour", "Salut", "Hey"];
+const subjects = ["le monde", "ami", "tout le monde"];
+const verbs = ["passe une bonne journee", "reste positif", "garde le sourire"];
 
+const randomGreetingIndex = Math.floor(Math.random() * greetings.length);
+const randomSubjectIndex = Math.floor(Math.random() * subjects.length);
+const randomVerbIndex = Math.floor(Math.random() * verbs.length);
 
 const message = `${greetings[randomGreetingIndex]}, ${subjects[randomSubjectIndex]}! ${verbs[randomVerbIndex]}`;
 
