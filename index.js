c42fed38f2de52afd98a542f50fb2a0632bdf377
Ezmuk <ezechielmukenge572@gmail.com>
2023-09-28 11:16:30 +0200

    creation tableaux

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
 
+const greetings = ["Bonjour", "Salut", "Hey"];
+const subjects = ["le monde", "ami", "tout le monde"];
+const verbs = ["passe une bonne journee", "reste positif", "garde le sourire"];
